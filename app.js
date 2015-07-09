4c84fab0cdecc4cdde2bf344d872a1b9f18ea350
sin <sinoory@126.com>
2015-07-09 22:03:38 +0800

    add testwebsocket

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var sio = require('socket.io');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+var testwebsocket=require('./routes/testwebsocket');
+app.use('/testwebsocket',testwebsocket);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,14 +60,23 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-io.on('connection', function(socket){
+var io=sio.listen(http);
+console.log("next process websocket");
+io.sockets.on('connection', function(socket){
+    console.log("connected");
     socket.on('login', function(obj){
         socket.name = obj.userid;
         io.emit('login', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
         console.log(obj.username+'加入了聊天室');
     });
+    
+    socket.on('connect', function(){
+        console.log("connect");
+    });
+
+
     socket.on('disconnect', function(){
+        console.log("disconnect");
     });
 
     socket.on('message', function(obj){
@@ -74,6 +85,7 @@ io.on('connection', function(socket){
     });
 
 });
+
 
 module.exports = app;
 
