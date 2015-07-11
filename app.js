638ae29dc3142c6a43e9f213306477074bbbccf8
sin <sinoory@126.com>
2015-07-11 10:38:32 +0800

    send postion/receive bettwen websocket ok

--- app.js
+++ app.js
@@ -11,19 +11,37 @@ var users = require('./routes/users');
 var app = express();
 var http = require('http').Server(app);
 
+//var users={};
+
 var WebSocket = require('faye-websocket');
 http.on('upgrade', function(request, socket, body) {
     if (WebSocket.isWebSocket(request)) {
         console.log("socket conneted");
         var ws = new WebSocket(request, socket, body);
-
+        var me="";
         ws.on('message', function(event) {
-            console.log("get mesg");
+            console.log("get msg="+event.data);
+            var msg=JSON.parse(event.data);
+            if(msg.type=="report"){
+                users[msg.user]={'ws':ws};
+                me=msg.user;
+            }else if(msg.type=="send"){
+               if(msg.to in users){
+                   var res=users[msg.to].ws.send(JSON.stringify(msg));
+                   console.log("msg send to "+msg.to+" finished,res="+res);
+               }else{
+                   console.log("user "+msg.to+" not exist");
+               }
+            }else{
+                console.log("not supported msg.type="+msg.type);
+            }
             //ws.send(event.data);
         });
 
         ws.on('close', function(event) {
             console.log('close', event.code, event.reason);
+            console.log(me+" closed");
+            delete users[me];
             ws = null;
         });
     }
