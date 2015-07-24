160709d31f408e4f5fa2ce0db71ef8e91d625f0a
sin <sinoory@126.com>
2015-07-24 16:40:18 +0800

    add hashdbopr in node_modules/tools/ , move busyness from app.js to busness.js

--- app.js
+++ app.js
@@ -14,36 +14,11 @@ var http = require('http').Server(app);
 //var users={};
 
 var WebSocket = require('faye-websocket');
+var BusBus = require("./busness.js");
+var busbus = new BusBus();
 http.on('upgrade', function(request, socket, body) {
     if (WebSocket.isWebSocket(request)) {
-        console.log("socket conneted");
-        var ws = new WebSocket(request, socket, body);
-        var me="";
-        ws.on('message', function(event) {
-            console.log("get msg="+event.data);
-            var msg=JSON.parse(event.data);
-            if(msg.type=="report"){
-                users[msg.user]={'ws':ws};
-                me=msg.user;
-            }else if(msg.type=="send"){
-               if(msg.to in users){
-                   var res=users[msg.to].ws.send(JSON.stringify(msg));
-                   console.log("msg send to "+msg.to+" finished,res="+res);
-               }else{
-                   console.log("user "+msg.to+" not exist");
-               }
-            }else{
-                console.log("not supported msg.type="+msg.type);
-            }
-            //ws.send(event.data);
-        });
-
-        ws.on('close', function(event) {
-            console.log('close', event.code, event.reason);
-            console.log(me+" closed");
-            delete users[me];
-            ws = null;
-        });
+        busbus.processNewWebCient(request, socket, body); 
     }
 });
 
---
+++ busness.js
@@ -0,0 +1,41 @@
+
+module.exports = function BusBus(){
+    var WebSocket = require('faye-websocket');
+
+    this.processNewWebCient=function(request, socket, body){
+        console.log("socket conneted");
+        var ws = new WebSocket(request, socket, body);
+        var me="";
+        ws.on('message', function(event) {
+            console.log("get msg="+event.data);
+            var msg=JSON.parse(event.data);
+            if(msg.type=="report"){
+                users[msg.user]={'ws':ws};
+                me=msg.user;
+            }else if(msg.type=="send"){
+               if(msg.to in users){
+                   var res=users[msg.to].ws.send(JSON.stringify(msg));
+                   console.log("msg send to "+msg.to+" finished,res="+res);
+               }else{
+                   console.log("user "+msg.to+" not exist");
+               }
+            }else{
+                console.log("not supported msg.type="+msg.type);
+            }
+            //ws.send(event.data);
+        });
+
+        ws.on('close', function(event) {
+            console.log('close', event.code, event.reason);
+            console.log(me+" closed");
+            delete users[me];
+            ws = null;
+        });
+
+    };
+
+    this.onReport=function(msg){
+    };
+
+}
+
